406e6adbdef36c5c9b8bf34d74ec6412abb55359
shiven217 <shivender.kvs@gmail.com>
2020-09-15 01:34:58 +0530

    Updates

---
+++ precache-manifest.e0ac90a12fa015f1593e93f9ecfd292e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58da39d64c29b68789932304ef1fce8a",
+    "url": "/FaceRecognition/index.html"
+  },
+  {
+    "revision": "a4ecbbcf8d99c3b4e799",
+    "url": "/FaceRecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c5eed30f5bfeb903fd92",
+    "url": "/FaceRecognition/static/css/main.3c1b37c5.chunk.css"
+  },
+  {
+    "revision": "a4ecbbcf8d99c3b4e799",
+    "url": "/FaceRecognition/static/js/2.768280e9.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/FaceRecognition/static/js/2.768280e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5eed30f5bfeb903fd92",
+    "url": "/FaceRecognition/static/js/main.277fe63c.chunk.js"
+  },
+  {
+    "revision": "a237c8afeb8aec7ea4a6",
+    "url": "/FaceRecognition/static/js/runtime-main.3fb5d59a.js"
+  }
+]);
